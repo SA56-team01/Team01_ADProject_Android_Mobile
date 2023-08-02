d4673a0ebb59583c3382475a6b1d69201885cae0
Jiayi <jiayi.gmail.com>
2023-08-03 00:08:35 +0800

    get location

--- app/src/main/java/nus/iss/spotifyteam1/Dashboard.java
+++ app/src/main/java/nus/iss/spotifyteam1/Dashboard.java
@@ -1,32 +1,55 @@
 package nus.iss.spotifyteam1;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
+import com.google.android.gms.location.CurrentLocationRequest;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class Dashboard extends AppCompatActivity {
-    private FusedLocationProviderClient fusedLocationClient;
+public class Dashboard extends AppCompatActivity{
     String[] loction = {Manifest.permission.ACCESS_FINE_LOCATION};
     int MY_REQ_LOCATION = 11;
+    LocationManager locationManager;
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
+        mContext=this;
+        locationManager=(LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
         getLocation();
     }
+    LocationListener locationListenerGPS=new LocationListener() {
+        @Override
+        public void onLocationChanged(android.location.Location location) {
+            double latitude=location.getLatitude();
+            double longitude=location.getLongitude();
+            String msg="New Latitude: "+latitude + "New Longitude: "+longitude;
+            Toast.makeText(mContext,msg,Toast.LENGTH_LONG).show();
+        }
+
+    };
+
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -40,22 +63,14 @@ public class Dashboard extends AppCompatActivity {
     }
 
     void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,loction,MY_REQ_LOCATION);
             return;
         }
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            double al= location.getAltitude();
-                            double la =  location.getLatitude();
-
-                        }
-                    }
-                });
+        locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER,
+                2000,
+                10, locationListenerGPS);
     }
+
+
 }
