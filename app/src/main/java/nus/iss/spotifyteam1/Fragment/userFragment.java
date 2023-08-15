b6a9849cdc4e53e07143c679dbbc9a97453da14f
Wang Xinji <wangxinji@88.com>
2023-08-15 15:55:26 +0800

    logout finished

--- app/src/main/java/nus/iss/spotifyteam1/Fragment/userFragment.java
+++ app/src/main/java/nus/iss/spotifyteam1/Fragment/userFragment.java
@@ -1,14 +1,17 @@
 package nus.iss.spotifyteam1.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+
+import nus.iss.spotifyteam1.LoginActivity;
 import nus.iss.spotifyteam1.R;
 
-public class userFragment extends Fragment {
+public class userFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,20 @@ public class userFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+
+        view.findViewById(R.id.logout).setOnClickListener(this);
+
+        return view;
     }
+
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.logout) {
+            Intent toLoginActivity = new Intent(getContext(), LoginActivity.class);
+            startActivity(toLoginActivity);
+            getActivity().finish();
+        }
+    }
+
 }
